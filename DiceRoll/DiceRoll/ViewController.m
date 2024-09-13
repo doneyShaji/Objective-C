80d4b807f25215481f7a8e66f437c523feea0d35
doneyShaji <doney.shaji@people10.com>
2024-09-13 09:39:50 +0530

    Clean Code

--- DiceRoll/DiceRoll/ViewController.m
+++ DiceRoll/DiceRoll/ViewController.m
@@ -29,53 +29,36 @@
     int randomNumber2 = arc4random_uniform((uint32_t)self.imageArray.count);
     
     [self rollFunctionality:randomNumber1 secondNumber:randomNumber2];
-    
 }
 
 - (void)rollFunctionality:(int)indexOne secondNumber:(int)indexTwo {
     NSLog(@"%d", indexOne);
     
     conditionSatisfied = NO;
-//    self.diceTwo.image = self.imageArray[indexTwo];
     
     if (indexArray.count != 0) {
         [self duplicateDiceLogic:indexArray indexNumber:indexOne];
-        NSLog(@" Count: %d", indexArray.count);
-            while (!conditionSatisfied) {
-                if ([indexArray containsObject:@(indexOne)]) {
-                    NSLog(@"%d", indexOne);
-                    self.diceOne.image = self.imageArray[indexOne];
-                    //Remove here
-                    [indexArray removeObject:@(indexOne)];
-                    NSLog(@"%@", indexArray);
-                    conditionSatisfied = YES;
-                } else {
-                    indexOne = arc4random_uniform((uint32_t)self.imageArray.count);
-                }
-            }
-        } else {
-            NSLog(@"zero array");
-            indexArray = [NSMutableArray arrayWithArray:@[@0, @1, @2, @3, @4, @5]];
-            NSLog(@" Count: %d", indexArray.count);
-                while (!conditionSatisfied) {
-                    if ([indexArray containsObject:@(indexOne)]) {
-                        NSLog(@"%d", indexOne);
-                        self.diceOne.image = self.imageArray[indexOne];
-                        //Remove here
-                        [indexArray removeObject:@(indexOne)];
-                        NSLog(@"%@", indexArray);
-                        conditionSatisfied = YES;
-                    } else {
-                        indexOne = arc4random_uniform((uint32_t)self.imageArray.count);
-                    }
-                }
-        }
-
-    
+    } else {
+        NSLog(@"zero array");
+        indexArray = [NSMutableArray arrayWithArray:@[@0, @1, @2, @3, @4, @5]];
+        [self duplicateDiceLogic:indexArray indexNumber:indexOne];
+    }
 }
 
 -(void) duplicateDiceLogic:(NSMutableArray *)array indexNumber:(int)diceNumber {
-    
+    NSLog(@" Count: %d", array.count);
+        while (!conditionSatisfied) {
+            if ([array containsObject:@(diceNumber)]) {
+                NSLog(@"%d", diceNumber);
+                self.diceOne.image = self.imageArray[diceNumber];
+                //Remove here
+                [array removeObject:@(diceNumber)];
+                NSLog(@"%@", array);
+                conditionSatisfied = YES;
+            } else {
+                diceNumber = arc4random_uniform((uint32_t)self.imageArray.count);
+            }
+        }
 }
 
 
